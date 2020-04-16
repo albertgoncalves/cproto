8ecc7b1332eaee6c744e2c34e0c70130c721bc03
albertgoncalves <goncalves.albert@gmail.com>
2020-04-16 13:04:24 -0400

    clang-format update, no-op

--- src/fifo.c
+++ src/fifo.c
@@ -8,12 +8,22 @@
 #define BOLD_UNDERLINE "\033[1;4m"
 #define CLOSE          "\033[0m"
 
-#define EXIT_IF(condition)                                                \
-    if (condition) {                                                      \
-        fprintf(stderr, "<%s%s%s>.%s%s%s() @%sline %d%s: %sExit%s", BOLD, \
-                __FILE__, CLOSE, BOLD, __func__, CLOSE, BOLD_UNDERLINE,   \
-                __LINE__, CLOSE, BOLD_PURPLE, CLOSE);                     \
-        exit(1);                                                          \
+#define EXIT_IF(condition)                                  \
+    if (condition) {                                        \
+        fprintf(stderr,                                     \
+                "<%s%s%s>.%s%s%s() @%sline %d%s: %sExit%s", \
+                BOLD,                                       \
+                __FILE__,                                   \
+                CLOSE,                                      \
+                BOLD,                                       \
+                __func__,                                   \
+                CLOSE,                                      \
+                BOLD_UNDERLINE,                             \
+                __LINE__,                                   \
+                CLOSE,                                      \
+                BOLD_PURPLE,                                \
+                CLOSE);                                     \
+        exit(1);                                            \
     }
 
 typedef uint8_t T;
