f2cc65249aa065d8860b030a30a30019fd1a76c9
Albert Goncalves <goncalves.albert@gmail.com>
2020-05-02 13:29:59 -0400

    proper functions

--- src/simd.c
+++ src/simd.c
@@ -18,17 +18,17 @@ int main(void) {
         7.0f,
         8.0f,
     };
-    float output[4];
-    lane_f32 a = _mm_setr_ps(input[3], input[2], input[1], input[0]);
-    lane_f32 b = _mm_broadcast_ss(&input[4]);
-    _mm_storeu_ps((float*)&output, _mm_mul_ps(a, b));
+    float    output[4];
+    lane_f32 a = _mm_setr_ps(input[0], input[1], input[2], input[3]);
+    lane_f32 b = _mm_load_ps(&input[4]);
+    _mm_store_ps((float*)&output, _mm_mul_ps(a, b));
     printf("%.2f\n"
            "%.2f\n"
            "%.2f\n"
            "%.2f\n",
-           (double)output[3],
-           (double)output[2],
+           (double)output[0],
            (double)output[1],
-           (double)output[0]);
+           (double)output[2],
+           (double)output[3]);
     return EXIT_SUCCESS;
 }
