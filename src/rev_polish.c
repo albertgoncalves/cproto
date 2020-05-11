0e72617d2572629b5b791b415c16aacdbbf22915
Albert Goncalves <goncalves.albert@gmail.com>
2020-05-10 20:31:28 -0400

    prevent bad things

--- src/rev_polish.c
+++ src/rev_polish.c
@@ -6,14 +6,11 @@
 typedef unsigned char u8;
 typedef float         f32;
 
-static f32 eval_rev_polish(const char* expression,
-                           u8          n,
-                           char*       buffer,
-                           f32*        stack) {
-    u8 stack_index = 0;
-    u8 buffer_index = 0;
-    for (u8 i = 0; i < n; ++i) {
-        char token = expression[i];
+static f32 eval_rev_polish(const char* expression, f32* stack, char* buffer) {
+    u8   stack_index = 0;
+    u8   buffer_index = 0;
+    char token = *expression++;
+    for (; token != '\0'; token = *expression++) {
         switch (token) {
         case '+': {
             if (stack_index < 2) {
@@ -65,18 +62,29 @@ static f32 eval_rev_polish(const char* expression,
                 continue;
             }
             stack[stack_index++] = strtof(buffer, NULL);
+            buffer[0] = '\0';
             buffer_index = 0;
             break;
         }
+        case '.': {
+            buffer[buffer_index++] = '.';
+            buffer[buffer_index] = '\0';
+            break;
+        }
         default: {
-            if ((('0' <= token) && (token <= '9')) || (token == '.')) {
+            if (('0' <= token) && (token <= '9')) {
                 buffer[buffer_index++] = token;
+                buffer[buffer_index] = '\0';
+            } else {
+                exit(EXIT_FAILURE);
             }
         }
         }
     }
-    f32 result = stack[0];
-    return result;
+    if ((stack_index != 1) || (buffer_index != 0)) {
+        exit(EXIT_FAILURE);
+    }
+    return stack[0];
 }
 
 int main(void) {
@@ -84,10 +92,8 @@ int main(void) {
     const char* expression = "3 4 2 * 1 5 - 2 3 ^ ^ / +";
     u8          n = (u8)strlen(expression);
     f32*        pool = calloc(sizeof(f32) + sizeof(char), n);
-    f32*        stack = pool;
-    char*       buffer = (char*)&pool[n];
-    f32         result = eval_rev_polish(expression, n, buffer, stack);
-    printf("%.8f\n", (double)result);
+    f32         result = eval_rev_polish(expression, pool, (char*)&pool[n]);
     free(pool);
+    printf("%.8f\n", (double)result);
     return EXIT_SUCCESS;
 }
