b5cb2bdb023533aa741539661f48bd1933365aeb
Albert Goncalves <goncalves.albert@gmail.com>
2020-05-25 01:08:54 -0400

    *seems* to work, but a ways to go...

--- src/regex.c
+++ src/regex.c
@@ -14,39 +14,36 @@ typedef enum {
 } Bool;
 
 typedef enum {
+    EPSILON = 0,
     TOKEN,
-    SPLIT,
 } StateType;
 
 typedef struct State State;
 
-typedef struct {
-    State* state_a;
-    State* state_b;
-} Split;
-
-typedef union {
-    State* state;
-    Split  split;
-} Flow;
-
 struct State {
-    Flow      next;
-    Flow      last;
-    char      token;
+    State*    next;
+    State*    next_split;
     StateType type;
+    char      token;
+    Bool      end;
 };
 
 typedef struct {
-    State  states[STATE_CAP];
-    State* stack[STACK_CAP];
-    State* stack_swap[STACK_CAP];
-    u8     state_len;
+    State* first;
+    State* last;
+} Link;
+
+typedef struct {
+    State states[STATE_CAP];
+    Link  stack_a[STACK_CAP];
+    Link  stack_b[STACK_CAP];
+    Link  stack_c[STACK_CAP];
+    u8    state_len;
 } Memory;
 
 typedef struct {
-    State** states;
-    u8      len;
+    Link* links;
+    u8    len;
 } Stack;
 
 static State* state_new(Memory* memory) {
@@ -56,124 +53,148 @@ static State* state_new(Memory* memory) {
     return &memory->states[memory->state_len++];
 }
 
-static State* get_nfa(Memory* memory, const char* postfix_expr) {
+static void set_token(Memory* memory, Stack* stack, char token) {
+    if (STACK_CAP <= stack->len) {
+        exit(EXIT_FAILURE);
+    }
+    Link link = {
+        .first = state_new(memory),
+        .last = state_new(memory),
+    };
+    link.last->end = TRUE;
+    link.first->type = TOKEN;
+    link.first->token = token;
+    link.first->next = link.last;
+    stack->links[stack->len++] = link;
+}
+
+static void set_concat(Stack* stack) {
+    if (stack->len < 2) {
+        exit(EXIT_FAILURE);
+    }
+    Link b = stack->links[--stack->len];
+    Link a = stack->links[--stack->len];
+    a.last->next = b.first;
+    a.last->end = FALSE;
+    Link link = {
+        .first = a.first,
+        .last = b.last,
+    };
+    stack->links[stack->len++] = link;
+}
+
+static Link get_nfa(Memory* memory, const char* postfix_expr) {
     Stack stack = {
-        .states = memory->stack,
+        .links = memory->stack_a,
         .len = 0,
     };
     for (char token = *postfix_expr++; token != '\0'; token = *postfix_expr++)
     {
         switch (token) {
         case OP_CONCAT: {
-            if (stack.len < 2) {
-                exit(EXIT_FAILURE);
-            }
-            State* b = stack.states[--stack.len];
-            State* a = stack.states[stack.len - 1];
-            switch (a->type) {
-            case TOKEN: {
-                if (a->last.state == NULL) {
-                    a->next.state = b;
-                    a->last.state = b;
-                } else {
-                    switch (a->last.state->type) {
-                    case TOKEN: {
-                        a->last.state->next.state = b;
-                        a->last.state->last.state = b;
-                        a->last.state = b;
-                        break;
-                    }
-                    case SPLIT: {
-                        break;
-                    }
-                    }
-                }
-                break;
-            }
-            case SPLIT: {
-                break;
-            }
-            }
+            set_concat(&stack);
             break;
         }
         default: {
-            if (STACK_CAP <= stack.len) {
-                exit(EXIT_FAILURE);
-            }
-            State* state = state_new(memory);
-            state->type = TOKEN;
-            state->token = token;
-            stack.states[stack.len++] = state;
+            set_token(memory, &stack, token);
         }
         }
     }
     if (stack.len != 1) {
         exit(EXIT_FAILURE);
     }
-    return stack.states[--stack.len];
+    return stack.links[0];
 }
 
-static Bool get_match(Memory* memory, State* nfa, const char* string) {
-    Stack stack = {
-        .states = memory->stack,
+static Bool get_match(Memory* memory, Link nfa, const char* string) {
+    Stack mixed_states = {
+        .links = memory->stack_a,
         .len = 0,
     };
-    Stack next = {
-        .states = memory->stack_swap,
+    Stack buffer = {
+        .links = memory->stack_b,
         .len = 0,
     };
-    stack.states[stack.len++] = nfa;
-    Bool end;
+    Stack nfa_tokens = {
+        .links = memory->stack_c,
+        .len = 0,
+    };
+    mixed_states.links[mixed_states.len++] = nfa;
     for (char token = *string++; token != '\0'; token = *string++) {
-        end = FALSE;
-        for (u8 i = 0; i < stack.len; ++i) {
-            State* state = stack.states[i];
-            switch (state->type) {
-            case TOKEN: {
-                if (state->token == token) {
-                    if (STACK_CAP <= next.len) {
-                        exit(EXIT_FAILURE);
+        for (;;) {
+            for (u8 i = 0; i < mixed_states.len; ++i) {
+                State* state = mixed_states.links[i].first;
+                switch (state->type) {
+                case EPSILON: {
+                    if (state->next != NULL) {
+                        buffer.links[buffer.len++].first = state->next;
                     }
-                    State* next_state = state->next.state;
-                    if (next_state != NULL) {
-                        next.states[next.len++] = next_state;
-                    } else {
-                        end = TRUE;
+                    if (state->next_split != NULL) {
+                        buffer.links[buffer.len++].first = state->next_split;
                     }
+                    break;
                 }
+                case TOKEN: {
+                    nfa_tokens.links[nfa_tokens.len++].first = state;
+                    break;
+                }
+                }
+            }
+            if (buffer.len == 0) {
                 break;
             }
-            case SPLIT: {
-                break;
-            }
-            }
+            Link* swap = mixed_states.links;
+            mixed_states.links = buffer.links;
+            mixed_states.len = buffer.len;
+            buffer.links = swap;
+            buffer.len = 0;
         }
-        State** swap = stack.states;
-        stack.states = next.states;
-        stack.len = next.len;
-        next.states = swap;
-        next.len = 0;
+        if (nfa_tokens.len == 0) {
+            return FALSE;
+        }
+        do {
+            State* state = nfa_tokens.links[--nfa_tokens.len].first;
+            if (token == state->token) {
+                if (*string == '\0') {
+                    return state->next->end;
+                }
+                if (state->next != NULL) {
+                    mixed_states.links[mixed_states.len++].first = state->next;
+                }
+                if (state->next_split != NULL) {
+                    mixed_states.links[mixed_states.len++].first =
+                        state->next_split;
+                }
+            }
+        } while (0 < nfa_tokens.len);
     }
-    return end;
+    return FALSE;
 }
 
 int main(void) {
     printf("sizeof(State)     : %lu\n"
+           "sizeof(u8)        : %lu\n"
            "sizeof(StateType) : %lu\n"
-           "sizeof(Memory)    : %lu\n",
+           "sizeof(char)      : %lu\n"
+           "sizeof(Bool)      : %lu\n"
+           "sizeof(Link)      : %lu\n"
+           "sizeof(Memory)    : %lu\n"
+           "\n",
            sizeof(State),
+           sizeof(u8),
            sizeof(StateType),
+           sizeof(char),
+           sizeof(Bool),
+           sizeof(Link),
            sizeof(Memory));
     Memory* memory = calloc(1, sizeof(Memory));
     if (memory == NULL) {
         return EXIT_FAILURE;
     }
-    // const char* postfix_expr = "abc|*.d.";
-    // const char* postfix_expr = "ab.c.";
-    // const char* input = "abc";
     const char* postfix_expr = "ab.c.d.";
     const char* input = "abcd";
-    Bool output = get_match(memory, get_nfa(memory, postfix_expr), input);
+    Link        nfa = get_nfa(memory, postfix_expr);
+    Bool        output = get_match(memory, nfa, input);
     printf("\"%s\" @ %s -> ", input, postfix_expr);
     switch (output) {
     case TRUE: {
