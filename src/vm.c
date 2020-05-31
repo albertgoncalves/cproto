accf44cc9ca79c30a5cd76d95a18c9c1e8582ae8
Albert Goncalves <goncalves.albert@gmail.com>
2020-05-31 15:28:55 -0400

    further clean-up

--- src/vm.c
+++ src/vm.c
@@ -49,31 +49,18 @@ static const InstrI32 PROGRAM[PROG_LEN] = {
     HALT,
 };
 
-#define INC1 ++i
-#define INC2 i = (u8)(i + 2)
-
+#define PRINT_ERROR_AND_EXIT            \
+    printf("\033[1;31mError\033[0m\n"); \
+    exit(EXIT_FAILURE)
+#define PRINT_INSTR(string) \
+    printf("    %2hhu - \033[1m%-6s\033[0m", i, string);
+#define PRINT_P1 printf("%4d\n", p1)
 #define POP_2                            \
     if (memory->stack_len < 2) {         \
-        exit(EXIT_FAILURE);              \
+        PRINT_ERROR_AND_EXIT;            \
     }                                    \
     i32 s1 = stack[--memory->stack_len]; \
     i32 s2 = stack[--memory->stack_len]
-
-#define JUMP_IF(x)                            \
-    if ((PROG_LEN - 1) <= i) {                \
-        exit(EXIT_FAILURE);                   \
-    }                                         \
-    if (memory->stack_len < 1) {              \
-        exit(EXIT_FAILURE);                   \
-    }                                         \
-    i32      s1 = stack[--memory->stack_len]; \
-    InstrI32 p1 = PROGRAM[i + 1];             \
-    printf("%3hhu - JUMP%d %3d\n", i, x, p1); \
-    if (s1 == x) {                            \
-        i = p1;                               \
-    } else {                                  \
-        INC2;                                 \
-    }
 
 static void run_program(Memory* memory) {
     i32* stack = memory->stack;
@@ -81,72 +68,88 @@ static void run_program(Memory* memory) {
         InstrI32 instruction = PROGRAM[i];
         switch (instruction) {
         case HALT: {
+            PRINT_INSTR("HALT");
             if (memory->stack_len != 1) {
-                exit(EXIT_FAILURE);
+                PRINT_ERROR_AND_EXIT;
             }
-            printf("%3hhu - HALT\n", i);
+            printf("\n");
             return;
         }
         case PUSH: {
+            PRINT_INSTR("PUSH");
             if ((STACK_CAP <= memory->stack_len) || ((PROG_LEN - 1) <= i)) {
-                exit(EXIT_FAILURE);
+                PRINT_ERROR_AND_EXIT;
             }
             i32 p1 = PROGRAM[i + 1];
-            printf("%3hhu - PUSH  %3d\n", i, p1);
+            PRINT_P1;
             stack[memory->stack_len++] = p1;
-            INC2;
+            i = (u8)(i + 2);
             break;
         }
         case POP: {
-            printf("%3hhu - POP\n", i);
+            PRINT_INSTR("POP");
             if (memory->stack_len < 1) {
-                exit(EXIT_FAILURE);
+                PRINT_ERROR_AND_EXIT;
             }
+            printf("\n");
             --memory->stack_len;
-            INC1;
+            ++i;
             break;
         }
         case COPY: {
-            printf("%3hhu - COPY\n", i);
+            PRINT_INSTR("COPY");
             if ((memory->stack_len < 1) || (STACK_CAP <= memory->stack_len)) {
-                exit(EXIT_FAILURE);
+                PRINT_ERROR_AND_EXIT;
             }
+            printf("\n");
             i32 s1 = stack[memory->stack_len - 1];
             stack[memory->stack_len++] = s1;
-            INC1;
+            ++i;
             break;
         }
         case JUMP0: {
-            JUMP_IF(0);
+            PRINT_INSTR("JUMP0");
+            if ((memory->stack_len < 1) || ((PROG_LEN - 1) <= i)) {
+                PRINT_ERROR_AND_EXIT;
+            }
+            InstrI32 p1 = PROGRAM[i + 1];
+            PRINT_P1;
+            if (stack[--memory->stack_len] == 0) {
+                i = p1;
+            } else {
+                i = (u8)(i + 2);
+            }
             break;
         }
         case LT: {
-            printf("%3hhu - LT\n", i);
+            PRINT_INSTR("LT");
             POP_2;
+            printf("\n");
             stack[memory->stack_len++] = s1 < s2 ? 1 : 0;
-            INC1;
+            ++i;
             break;
         }
         case ADD: {
-            printf("%3hhu - ADD\n", i);
+            PRINT_INSTR("ADD");
             POP_2;
+            printf("\n");
             stack[memory->stack_len++] = s1 + s2;
-            INC1;
+            ++i;
             break;
         }
         default: {
-            printf("%3hhu - ?\n", i);
-            exit(EXIT_FAILURE);
+            PRINT_INSTR("?");
+            PRINT_ERROR_AND_EXIT;
         }
         }
     }
     return;
 }
 
-#undef INC1
-#undef INC2
+#undef PRINT_ERROR_AND_EXIT
+#undef PRINT_INSTR
+#undef PRINT_P1
 #undef POP_2
-#undef JUMP_IF
 
 int main(void) {
     printf("\n"
@@ -166,7 +169,9 @@ int main(void) {
         return EXIT_FAILURE;
     }
     run_program(memory);
-    printf("\nmemory->stack[0] : %d\n", memory->stack[0]);
+    printf("\nmemory->\033[1mstack\033[0m[\033[1m0\033[0m] : "
+           "\033[1;34m%d\033[0m\n",
+           memory->stack[0]);
     free(memory);
     return EXIT_SUCCESS;
 }
