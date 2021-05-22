4eaa9c9831803e0f260a0835fb5f7468b2751959
Albert Goncalves <goncalves.albert@gmail.com>
2021-05-22 15:30:27 -0400

    xdg-open -> feh

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 mkShell.override { stdenv = llvmPackages_11.stdenv; } {
     buildInputs = [
         cppcheck
+        feh
         gdb
         shellcheck
         valgrind
