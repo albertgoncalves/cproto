cd3224e56a4a1564404b66645510db91433c57cf
albertgoncalves <goncalves.albert@gmail.com>
2022-04-04 10:00:09 -0400

    remove unused deps

--- shell.nix
+++ shell.nix
@@ -2,9 +2,7 @@ with import <nixpkgs> {};
 mkShell.override { stdenv = llvmPackages_14.stdenv; } {
     buildInputs = [
         feh
-        gdb
         shellcheck
-        valgrind
         xorg.libX11
     ];
     shellHook = ''
