fa210a00e258c8d7d81688746ec7d485f2e91716
albertgoncalves <goncalves.albert@gmail.com>
2024-04-17 21:30:28 -0400

    improve backtrace demo

--- src/exit_with_trace.c
+++ src/exit_with_trace.c
@@ -1,6 +1,6 @@
 #include <execinfo.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 typedef int32_t i32;
@@ -8,23 +8,35 @@ typedef int32_t i32;
 #define OK    0
 #define ERROR 1
 
-#define BUFFER_CAP 10
+#define BUFFER_CAP 16
+
+#define TRACE()                                           \
+    do {                                                  \
+        void*     buffer[BUFFER_CAP];                     \
+        const i32 n = backtrace(buffer, BUFFER_CAP);      \
+        char**    symbols = backtrace_symbols(buffer, n); \
+        if (symbols) {                                    \
+            for (i32 i = 0; i < n; ++i) {                 \
+                fprintf(stderr, "\t%s\n", symbols[i]);    \
+            }                                             \
+        }                                                 \
+    } while (0)
 
 #define EXIT_WITH_TRACE()                                            \
     do {                                                             \
+        fflush(stdout);                                              \
+        fflush(stderr);                                              \
         fprintf(stderr, "%s:%s:%d\n", __FILE__, __func__, __LINE__); \
-        void*  buffer[BUFFER_CAP];                                   \
-        i32    n = backtrace(buffer, BUFFER_CAP);                    \
-        char** symbols = backtrace_symbols(buffer, n);               \
-        if (symbols) {                                               \
-            for (i32 i = 0; i < n; ++i) {                            \
-                fprintf(stderr, "\t%s\n", symbols[i]);               \
-            }                                                        \
-        }                                                            \
+        TRACE();                                                     \
         _exit(ERROR);                                                \
     } while (0)
 
-i32 main(void) {
+__attribute__((noreturn)) void some_function(void);
+__attribute__((noreturn)) void some_function(void) {
     EXIT_WITH_TRACE();
+}
+
+i32 main(void) {
+    some_function();
     return OK;
 }
