28a8239cbe83f7457d16793a4539d053ca800fc8
albertgoncalves <goncalves.albert@gmail.com>
2025-08-17 22:03:31 -0400

    remove indirection from lazy object

--- src/lazy.c
+++ src/lazy.c
@@ -4,49 +4,49 @@
 
 typedef int64_t i64;
 
-typedef struct {
-    void* (*func)(void*);
-    void* args;
-} Closure;
+#define CODEGEN(A, B)                                                        \
+    typedef struct Lazy_##A##_##B {                                          \
+        B (*eval)(struct Lazy_##A##_##B*);                                   \
+        union {                                                              \
+            struct {                                                         \
+                B (*func)(A);                                                \
+                A args;                                                      \
+            } closure;                                                       \
+            B result;                                                        \
+        } internal;                                                          \
+    } Lazy_##A##_##B;                                                        \
+                                                                             \
+    static B lazy_from_cache_##A##_##B(Lazy_##A##_##B* lazy) {               \
+        return lazy->internal.result;                                        \
+    }                                                                        \
+                                                                             \
+    static B lazy_eval_##A##_##B(Lazy_##A##_##B* lazy) {                     \
+        B result = lazy->internal.closure.func(lazy->internal.closure.args); \
+        lazy->eval = lazy_from_cache_##A##_##B;                              \
+        lazy->internal.result = result;                                      \
+        return result;                                                       \
+    }
 
-typedef struct Lazy Lazy;
+typedef char* String;
 
-struct Lazy {
-    void* (*eval)(Lazy*);
-    Closure closure;
-};
+CODEGEN(String, i64)
 
-static void* lazy_from_cache(Lazy* lazy) {
-    return lazy->closure.args;
-}
-
-static void* lazy_eval(Lazy* lazy) {
-    void* result = lazy->closure.func(lazy->closure.args);
-    lazy->eval = lazy_from_cache;
-    lazy->closure.args = result;
-    return result;
-}
-
-static void* func(void*) {
+static i64 func(String message) {
+    printf("%s\n", message);
     usleep(500000);
-    return (void*)-123;
+    return -123;
 }
 
 int main(void) {
-    printf("sizeof(Closure) : %zu\n"
-           "sizeof(Lazy)    : %zu\n"
-           "\n",
-           sizeof(Closure),
-           sizeof(Lazy));
+    printf("sizeof(Lazy_String_i64): %zu\n", sizeof(Lazy_String_i64));
 
-    Lazy lazy = (Lazy){
-        .eval = lazy_eval,
-        .closure = {.func = func, .args = NULL},
+    Lazy_String_i64 lazy = (Lazy_String_i64){
+        .eval = lazy_eval_String_i64,
+        .internal = {.closure = {.func = func, .args = "?!"}},
     };
 
     for (int i = 0; i < 3; ++i) {
-        void* result = lazy.eval(&lazy);
-        printf("%ld\n", (i64)result);
+        printf("%ld\n", lazy.eval(&lazy));
     }
 
     return 0;
