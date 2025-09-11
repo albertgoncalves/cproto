75037992569b5570b0c4c1e507e205777ac996f6
albertgoncalves <goncalves.albert@gmail.com>
2025-09-10 21:17:27 -0400

    simplify and (slightly) optimize

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,34 +1,31 @@
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef uint8_t u8;
+static void insertion_sort(int* array, int n) {
+    for (int i = 0; i < n; ++i) {
+        const int x = array[i];
 
-typedef int32_t i32;
-
-static void insertion_sort_asc(u8* array, u8 n) {
-    for (u8 i = 0; i < n; ++i) {
-        for (u8 j = i; (0 < j) && (array[j] < array[j - 1]); --j) {
-            u8 t = array[j];
+        int j = i;
+        for (; (0 < j) && (x < array[j - 1]); --j) {
             array[j] = array[j - 1];
-            array[j - 1] = t;
         }
+        array[j] = x;
     }
 }
 
-static void print(u8* array, u8 n) {
-    printf("[");
-    for (u8 i = 0; i < n; ++i) {
-        printf(" %hhu", array[i]);
+static void print(int* array, int n) {
+    for (int i = 0; i < n; ++i) {
+        printf(" %2hhu", array[i]);
     }
-    printf(" ]\n");
+    putchar('\n');
 }
 
-i32 main(void) {
-    u8 array[] = {0, 5, 3, 6, 4, 7, 8, 1, 0};
-    u8 n = sizeof(array) / sizeof(array[0]);
-    print(array, n);
-    insertion_sort_asc(array, n);
-    print(array, n);
-    return EXIT_SUCCESS;
+int main(void) {
+    int array[] = {14, 13, 0, 5, 3, 12, 6, 4, 9, 2, 7, 8, 1, 11, 10};
+
+#define N (sizeof(array) / sizeof(array[0]))
+    print(array, N);
+    insertion_sort(array, N);
+    print(array, N);
+
+    return 0;
 }
